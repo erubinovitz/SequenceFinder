1cd01e85741b992c7fb0d3f5e4e8ddd4770e4f31
erubinovitz <erubinovitz@yahoo.com>
2020-01-30 02:42:12 -0500

    Code Dump

--- dnaSequenceFinder/dnaSequenceFinder/dnaSequenceFinder/static/JavaScript/homePageJS.js
+++
@@ -1,6 +0,0 @@
-
-function addSequence(){
-	document.getElementById("sequenceList").setInnerHTML(document.getElementById("sequenceList") + "\nAAAA");
-	console.log("here");
-	alert("test")
-}
--- dnaSequenceFinder/homePage/views.py
+++ dnaSequenceFinder/homePage/views.py
@@ -11,10 +11,9 @@ def homePage(request):
         form = inputForm(request.POST)
         if form.is_valid():
             seq = findProtein(form.cleaned_data["sequence"]);
-            # seq.save()
+            seq.save()
             context = {"sequenceName": seq.sequence,"proteinName":seq.proteinName,"proteinIndex":seq.proteinIndex, "form": form}
             return render(request, "homePage.html", context)
-        else:
-            print("req not valid req is ", request.method);
+
     context = {"sequence": sequence, "form": form}
     return render(request, "homePage.html", context)
